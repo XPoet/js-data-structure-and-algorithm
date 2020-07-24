e22ba49b45505cb6dacc071c2bd323c9e1f69207
XPoet <i@xpoet.cn>
2020-07-24 17:58:39 +0800

    控制台增加栈结构测试的起始点标记

--- src/stack/index.js
+++ src/stack/index.js
@@ -2,6 +2,7 @@ import Stack from './stack';
 import dec2bin from './dec2bin';
 
 // ----- 栈结构测试 -----//
+console.log('// ----- 栈结构测试 START -----//');
 const stack = new Stack();
 
 // push() 方法测试
@@ -29,5 +30,7 @@ console.log(stack.toString());
 // 利用栈结构的特点实现十进制转换为二进制的方法测试
 console.log(dec2bin(100));
 console.log(dec2bin(88));
+
+console.log('// ----- 栈结构测试 END -----//');
 
 
